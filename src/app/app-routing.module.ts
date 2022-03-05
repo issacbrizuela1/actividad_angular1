f7cba65f839a416431b6fac696b52553cab97735
echodegrasa <74998947+echodegrasa@users.noreply.github.com>
2022-03-05 13:23:02 -0600

    configuracion del modulo de ruteo y configuracion del servicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',redirectTo:'/auth',pathMatch:'full'},
+{path:'auth', loadChildren: () => AuthModule}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IUser } from '../modelos/user';
+import { IJWTResponse } from '../modelos/jwt-response';
+import { tap } from 'rxjs';
+import { Observable,BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
+  AUTH_SERVER:String='link del servidor';
+  authSubject=new BehaviorSubject(false);
+  private token:string | undefined;
+  constructor(private httpClient:HttpClient) {}
+  register(user:IUser):Observable<IJWTResponse>
+  {
+    return this.httpClient.post<IJWTResponse>('${this.AUTH_SERVER}/register',user).pipe(tap((res:IJWTResponse)=>{
+      if(res)
+      {
+        //guardar token
+      }
+    }));
+  }
 }
