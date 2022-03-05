21f80b337db6bc528e68d1bed42b06143fd74a4f
echodegrasa <74998947+echodegrasa@users.noreply.github.com>
2022-03-05 11:34:10 -0600

    configuracion del modulo auth

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginComponent,RegisterComponent],
+  imports: [CommonModule,FormsModule,HttpClientModule,AuthRoutingModule]
 })
 export class AuthModule { }
